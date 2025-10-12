6cdfb6e64113cd156bc3d2ba5ba21759160d5de3
Jarl Dang <jarldang@gmail.com>
2025-10-12 20:03:32 +0200

    LGP CHROMOSOMES

---
+++ HP2/HP2.4/checkpoints/best_chromosome_gen14000.py
@@ -0,0 +1,6 @@
+# Auto-generated best chromosome file
+VARIABLE_REGS = 6
+CONST_VALUES = [0.0, 1.0, -1.0, 2.0, -2.0, 0.5]
+OPERATORS = ['+', '-', '*', '/']
+CHROMOSOME = [3, 2, 0, 2, 0, 5, 2, 7, 0, 0, 0, 5, 0, 0, 0, 5, 2, 2, 11, 0, 2, 2, 2, 2, 0, 5, 2, 7, 3, 0, 0, 5]  # flat list; 4 genes per instruction
+BEST_RMSE = 0.1425263867647293
