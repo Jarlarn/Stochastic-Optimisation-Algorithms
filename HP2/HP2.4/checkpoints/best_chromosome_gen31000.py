21ca51ac06c0903dc6a75ba6e06ce70833acef5c
Jarl Dang <jarldang@gmail.com>
2025-10-15 03:48:29 +0200

    cleanup

--- HP2/HP2.4/checkpoints/best_chromosome_gen31000.py
+++
@@ -1,6 +0,0 @@
-# Auto-generated best chromosome file
-VARIABLE_REGS = 6
-CONST_VALUES = [0.0, 1.0, -1.0, 2.0, -2.0, 0.5]
-OPERATORS = ['+', '-', '*', '/']
-CHROMOSOME = [0, 5, 0, 7, 0, 0, 0, 5, 0, 0, 0, 5, 2, 5, 11, 5, 2, 3, 11, 0, 0, 0, 0, 5, 2, 2, 11, 0, 2, 2, 3, 2, 0, 5, 2, 7, 3, 0, 0, 5]  # flat list; 4 genes per instruction
-BEST_RMSE = 0.12324594888087777
